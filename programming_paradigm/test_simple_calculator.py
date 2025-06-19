9b43ef3c2aacc362a3f9d1ab30e9be59ab079646
Lindah <linihim03@gmail.com>
2025-06-19 12:39:21 +0200

    Correct the indentation problem in test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,14 +10,17 @@ class TestSimpleCalculator(unittest.TestCase):
 		self.assertEqual(self.calc.add(-1, 1), 0)
 		self.assertEqual(self.calc.add(-1, -1), -2)
 	def test_subtraction(self):
+#  self.assertEqual(self.calc.subtract(5, 2), 3)
 		self.assertEqual(self.calc.subtract(5, 2), 3)
 		self.assertEqual(self.calc.subtract(-5, -2), -3)
 		self.assertEqual(self.calc.subtract(5, -2), 7)
 	def test_multiplication(self):
+# self.assertEqual(self.calc.multiply(2, 3), 6)
 		self.assertEqual(self.calc.multiply(2, 3), 6)
 		self.assertEqual(self.calc.multiply(-2, -3), 6)
 		self.assertEqual(self.calc.multiply(5, 0), 0)
 	def test_division(self):
+#  self.assertEqual(self.calc.divide(10, 5), 2.0)
 		self.assertEqual(self.calc.divide(10, 5), 2.0)
 		self.assertEqual(self.calc.divide(0, 5), 0.0)
 		self.assertIsNone(self.calc.divide(10, 0))
